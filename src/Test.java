9adf0d104b122fc409bb0418772fcf389c136f94
Peter <peter.mueckstein@gmail.com>
2017-12-12 21:54:46 +0100

    Second Commit

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,9 @@
 public class Test {
     public static void main(String[]args){
-        String[] m = new String[]{"Peter","Johannes"};
+        String[] m = new String[]{"felix","Theodor"};
         Arryswords a = new Arryswords();
         for (int i = 0;i < m.length;i++)
-        System.out.println(a.getLengths(m).toString());
+        System.out.println(a.getLengths(m)[i]);
         }
     }
 
