2a167cb5db552753651d6713cc7c58f7352207bc
Peter <peter.mueckstein@gmail.com>
2017-12-13 16:42:05 +0100

    Test die Lotto und Hashset

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,41 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Test {
     public static void main(String[]args){
-        String[] m = new String[]{"felix","Theodor"};
+        /*String[] m = new String[]{"felix","Theodor"};
         Arryswords a = new Arryswords();
         for (int i = 0;i < m.length;i++)
         System.out.println(a.getLengths(m)[i]);
+        */
+        Scanner s = new Scanner(System.in);
+        Random rnd = new Random();
+        Set<Integer> lotto = new HashSet<Integer>();
+        Set<Integer> numbers = new HashSet<>();
+        for (int i = 0;i < 6;i++){
+            System.out.print("Bitte gib eine Lotto Zahhl ein");
+            numbers.add(s.nextInt());
+        }
+        int i = 0;
+        while (true){
+        do {
+            lotto.add(1 + rnd.nextInt(49));
+            }while (lotto.size() < 6);
+            Object[] lottoarray = lotto.toArray();
+            Object[] numberarray = numbers.toArray();
+            Arrays.sort(numberarray);
+            Arrays.sort(lottoarray);
+            System.out.println(Arrays.toString(lottoarray));
+
+
+            System.out.println(Arrays.toString(numberarray) + i);
+            i++;
+
+            if (lottoarray == numberarray){
+                break;
+                }
+             lotto.clear();
+            }
         }
     }
 
