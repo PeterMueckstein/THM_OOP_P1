0e3637946922b213716ee028fe294d616d69ba90
Peter Mückstein <peter.mueckstein@gmail.com>
2018-04-19 16:52:30 +0200

    Neu

--- Lernen/src/Müll5.java
+++ Lernen/src/Müll5.java
@@ -4,5 +4,22 @@ public class Müll5 {
     public static void main(String[]args){
         System.out.print("Bitte geben sie Die höhe und den durchmesser ein");
         Scanner s = new Scanner(System.in);
+        double h;
+        double d;
+        double r;
+        double umfang;
+        double flaechboden;
+        double mantelflaeche;
+        double flaeche;
+        double v;
+        h = s.nextDouble();
+        d = s.nextDouble();
+        r = d /2;
+        umfang = d * Math.PI;
+        flaechboden = Math.PI * (r*r);
+        mantelflaeche = 2 * Math.PI * r * (r + h);
+        flaeche = 2 * flaechboden + mantelflaeche;
+        v = Math.PI * (r*r) * h;
+        System.out.print(" Die Dose hat dem Umfang von : " + umfang + "\n Höhe :" + h + "\n Durchmesser vom Boden: " + d + "\n Fläche vom Boden: " + flaechboden + "\n Mantelfläche: " + mantelflaeche + "\n Gesamtfläche: " + flaeche + "\n Volumen: "+ v);
     }
 }
--- Lernen/src/Müll6.java
+++ Lernen/src/Müll6.java
@@ -1,8 +1,16 @@
-import java.util.Random;
-
 public class Müll6 {
-    public static void Main(String[]args){
-        Random r = new Random();
-
+    public static void main(String[]args){
+     double[] d = new double[20];
+     for (int i = 0;i < 20;i++){
+         d[i] = Math.random();
+     }
+     double b=0;
+     for (int i = 0;i < 20;i++){
+         if (d[i] > b){
+             b = d[i];
+         }
+            System.out.println(d[i]);
+     }
+     System.out.println("Die größte Zahl ist " + b);
     }
 }
